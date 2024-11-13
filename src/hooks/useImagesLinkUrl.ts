80f5e3d5b853f78a82d025c8a93476253b492e98
Hammad Asif <mrhammadasif@gmail.com>
2024-11-13 23:20:04 +0500

    Update the assets URL to use the basePath and assets folder

--- src/hooks/useImagesLinkUrl.ts
+++ src/hooks/useImagesLinkUrl.ts
@@ -1,16 +1,10 @@
-import { useContext } from "react"
-import { TEEAuthDataContext } from "../context"
+import { useConfig } from "../context"
 
 const useImagesLinkUrl = () => {
-    const { edxAppConfig } = useContext(TEEAuthDataContext)
+    const { config }= useConfig()
 
     const getAssetsUrl = () => {
-        // if (edxAppConfig && edxAppConfig.app && edxAppConfig.app.baseImagesUrl)
-        //     return edxAppConfig.app.baseImagesUrl
-
-        // console.error("baseImagesUrl missing in config. You have to include the app.baseImagesUrl property")
-
-        return ""
+        return `${config.app.basePath}/assets`
     }
 
     return {
