01f214b06a224935124b2c980e943dbad9821acc
Hammad Asif <mrhammadasif@gmail.com>
2024-11-15 00:52:27 +0500

    export the tenant hook

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -1,33 +1,24 @@
-import useAppBookmarks from "./useAppBookmarks";
-import useAuthAutoRefresh from "./useAuthAutoRefresh";
-import useUserAppList from "./useUserAppList";
-import useUserProfile from "./useUserProfile";
-import useAuthActions from "./useAuthActions";
-import useEdxAppConfig from "./useEdxAppConfig";
-import useFetchEdxAppConfig from "./useFetchEdxAppConfig";
-import useIdleSession from "./useIdleSession";
-import useLoadingState from "./useLoadingState";
-import useNotificationsBar from "./useNotificationBar";
-import useRouteLayoutSelector from "./useRouteLayoutSelector";
-import useSideMenu from "./useSideMenu";
-import useAnalyticsTagManager from "./useAnalyticsTagManager";
-import useInitialRoute from "./useInitialRoute";
-import useSaveInitialRoute from "./useSaveInitialRoute";
+import useAnalyticsTagManager from "./useAnalyticsTagManager"
+import useAppBookmarks from "./useAppBookmarks"
+import useAuthActions from "./useAuthActions"
+import useAuthAutoRefresh from "./useAuthAutoRefresh"
+import useEdxAppConfig from "./useEdxAppConfig"
+import useFetchEdxAppConfig from "./useFetchEdxAppConfig"
+import useIdleSession from "./useIdleSession"
+import useInitialRoute from "./useInitialRoute"
+import useLoadingState from "./useLoadingState"
+import useNotificationsBar from "./useNotificationBar"
+import useRouteLayoutSelector from "./useRouteLayoutSelector"
+import useSaveInitialRoute from "./useSaveInitialRoute"
+import useSideMenu from "./useSideMenu"
+import useTenantSelectPopover from './useTenantSelectPopover'
+import useUserAppList from "./useUserAppList"
+import useUserProfile from "./useUserProfile"
 
 export {
-    useEdxAppConfig,
-    useFetchEdxAppConfig,
-    useIdleSession,
-    useLoadingState,
-    useNotificationsBar,
-    useInitialRoute,
-    useSaveInitialRoute,
-    useRouteLayoutSelector,
-    useSideMenu,
-    useAnalyticsTagManager,
-    useAuthAutoRefresh,
-    useUserAppList,
-    useUserProfile,
-    useAuthActions,
-    useAppBookmarks
+  useAnalyticsTagManager, useAppBookmarks, useAuthActions, useAuthAutoRefresh, useEdxAppConfig,
+  useFetchEdxAppConfig,
+  useIdleSession, useInitialRoute, useLoadingState,
+  useNotificationsBar, useRouteLayoutSelector, useSaveInitialRoute, useSideMenu, useTenantSelectPopover, useUserAppList,
+  useUserProfile
 }
